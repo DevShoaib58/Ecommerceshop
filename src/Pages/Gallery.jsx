00a4819444f2e6bb35fcbddc7568c7bb63f274e1
Shoaib Qureshi <shoaib@solutionsurface.com>
2023-06-12 18:02:22 +0500

    Adding New Gallery Componet in Project

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -1,30 +1,29 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Gallery = () => {
-    const [data, setData] = useState([])
-    const [img, setImg] = useState([])
-    const fatchdata = async () => {
-        fatchdata(`https://api.unsplash.com/search/photos?page=1&query=${img}&client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}`)
-            // fatchdata("https://api.unsplash.com/photos/?client_id=eJrGowlILBc7vmgyiBxiazUp8aXES8G-pA3yuxGQpis")
-            .then(res => {
-                return res.json()
+    const [img, setImg] = useState("car")
+    const [res, setRes] = useState([])
+
+    const fetchAPI = () => {
+        fetch(`https://api.unsplash.com/search/photos?page=1&query=${img}&client_id=eJrGowlILBc7vmgyiBxiazUp8aXES8G-pA3yuxGQpis`)
+            .then(response => {
+                return response.json()
             })
             .then(data => {
-                setData(data)
-                console.log('data: ', data);
-            }
-            )
+                setRes(data.results)
+                console.log('res: ', res);
+            })
+
     }
     useEffect(() => {
-        fatchdata();
+        fetchAPI();
     }, [])
-
-
-
+    const Submit = () => {
+        fetchAPI();
+        setImg("car");
+    }
     return (
         <>
-
             <section className="text-gray-600 body-font">
                 <div className="container px-5 py-24 mx-auto ">
                     <div className="w-full">
@@ -32,41 +31,22 @@ const Gallery = () => {
                     </div>
                     <div className="flex w-full justify-center items-end mb-10">
                         <div className="relative mr-4 lg:w-full xl:w-1/2 w-2/4 md:w-full text-left">
-                            <label for="hero-field" className="leading-7 text-sm text-gray-600">Placeholder</label>
-                            <input type="text" value={img} onChange={(e) => setImg(e.target.value)} name="hero-field" className="w-full bg-gray-100 bg-opacity-50 rounded focus:ring-2 focus:ring-indigo-200 focus:bg-transparent border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                            <label className="leading-7 text-sm text-gray-600">Search Here</label>
+                            <input value={img} onChange={(e) => setImg(e.target.value)} type="text" name="hero-field" className="w-full bg-gray-100 bg-opacity-50 rounded focus:ring-2 focus:ring-indigo-200 focus:bg-transparent border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                         </div>
-                        <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+                        <button onClick={Submit} className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
                     </div>
                     <div className="flex flex-wrap md:-m-2 -m-1">
-                        <div className="flex flex-wrap w-1/2">
-                            <div className="md:p-2 p-1 w-1/2">
-                                <img alt="gallery" className="w-full object-cover h-full object-center block" src="https://dummyimage.com/500x300" />
-                            </div>
-                            <div className="md:p-2 p-1 w-1/2">
-                                <img alt="gallery" className="w-full object-cover h-full object-center block" src="https://dummyimage.com/500x300" />
-                            </div>
-                            <div className="md:p-2 p-1 w-1/2">
-                                <img alt="gallery" className="w-full object-cover h-full object-center block" src="https://dummyimage.com/500x300" />
-                            </div>
-                            <div className="md:p-2 p-1 w-1/2">
-                                <img alt="gallery" className="w-full object-cover h-full object-center block" src="https://dummyimage.com/500x300" />
-                            </div>
+                        <div className="flex flex-wrap w-100">
+                            {res.map((obj) => {
+                                return (
+                                    <div key={obj.id} className="md:p-2 p-1 w-1/2">
+                                        <img className="w-full object-cover h-full object-center block" src={obj.urls.small} alt="obj.alt_description" />
+                                        {/* <img className="w-full object-cover h-full object-center block" src="https://placehold.co/600x400" alt="val.alt_description" /> */}
+                                    </div>
+                                )
+                            })}
 
-                        </div>
-                        <div className="flex flex-wrap w-1/2">
-
-                            <div className="md:p-2 p-1 w-1/2">
-                                <img alt="gallery" className="w-full object-cover h-full object-center block" src="https://dummyimage.com/500x300" />
-                            </div>
-                            <div className="md:p-2 p-1 w-1/2">
-                                <img alt="gallery" className="w-full object-cover h-full object-center block" src="https://dummyimage.com/500x300" />
-                            </div>
-                            <div className="md:p-2 p-1 w-1/2">
-                                <img alt="gallery" className="w-full object-cover h-full object-center block" src="https://dummyimage.com/500x300" />
-                            </div>
-                            <div className="md:p-2 p-1 w-1/2">
-                                <img alt="gallery" className="w-full object-cover h-full object-center block" src="https://dummyimage.com/500x300" />
-                            </div>
                         </div>
                     </div>
                 </div>
