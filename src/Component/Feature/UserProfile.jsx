52f75659fc1d6923a141d24881edefa6971b00ad
Shoaib Qureshi <shoaib@solutionsurface.com>
2023-06-23 17:04:05 +0500

    Fixing API Fatching issue

--- src/Component/Feature/UserProfile.jsx
+++ src/Component/Feature/UserProfile.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 const UserProfile = () => {
-    const [block, setBlock] = useState({ gender: "male", name: { title: "Mr", first: "no name", last: "no name" }, cell: "987789879879", picture: { large: "https://static.vecteezy.com/system/resources/thumbnails/006/487/917/small/man-avatar-icon-free-vector.jpg" } })
+    const [block, setBlock] = useState('')
 
     const fetchUserData = () => {
         fetch("https://randomuser.me/api/")
@@ -19,21 +19,22 @@ const UserProfile = () => {
     }, [])
     return (
         <div>
-            <section style={{ background: block.gender == "male" ? 'blue' : 'pink', width: "500px" }} className="mx-auto text-gray-600 body-font m-4">
-                <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-                    <img className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src={block.picture.large} />
-                    <div className="text-center lg:w-2/3 w-full">
-                        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Gender : {block.gender}</h1>
-                        <p className="mb-4 leading-relaxed text-white">Title : {block.name.title}</p>
-                        <p className="mb-4 leading-relaxed text-white"> {block.cell}</p>
-                        <div className="flex justify-center">
-                            <button onClick={() => { fetchUserData() }} className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Refresh</button>
+            {block &&
+                <section style={{ background: block.gender == "male" ? 'blue' : 'pink', width: "500px" }} className="mx-auto text-gray-600 body-font m-4">
+                    <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+                        <img className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src={block.picture.large} />
+                        <div className="text-center lg:w-2/3 w-full">
+                            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Gender : {block.gender}</h1>
+                            <p className="mb-4 leading-relaxed text-white">Title : {block.name.title}</p>
+                            <p className="mb-4 leading-relaxed text-white"> {block.cell}</p>
+                            <div className="flex justify-center">
+                                <button onClick={() => { fetchUserData() }} className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Refresh</button>
 
+                            </div>
                         </div>
                     </div>
-                </div>
-            </section>
-
+                </section>
+            }
         </div>
     );
 };
--- src/Pages/Quote.jsx
+++ src/Pages/Quote.jsx
@@ -1,31 +1,31 @@
 import { useEffect, useState } from "react";
 
 const Quote = () => {
-    const [quote, setQuote] = useState(null)
+    const [quote, setQuote] = useState('')
 
     const fetchUserData = async () => {
-        fetch("https://api.adviceslip.com/advice")
+        await fetch("https://api.adviceslip.com/advice")
             .then(response => {
                 return response.json()
             })
             .then(data => {
-                setQuote(data)
+                setQuote(data.slip)
                 console.log('data: ', data);
             })
     }
+    const { id, advice } = quote;
 
     useEffect(() => {
         fetchUserData()
     }, [])
     return (
         <>
-
             <section className="text-gray-600 body-font">
                 <div className="container px-5 py-24 mx-auto">
                     <h1 className="sm:text-3xl text-2xl font-medium title-font text-center mb-10 text-gray-900">Quote</h1>
                     <div className="lg:w-2/3 flex flex-col sm:flex-row sm:items-center items-start mx-auto">
-                        <h1 className="flex-grow sm:pr-16 text-2xl font-medium title-font text-gray-900">{quote == null ? <h1>No Data</h1> : quote.slip.id}</h1>
-                        <h1 className="flex-grow sm:pr-16 text-2xl font-medium title-font text-gray-900">{quote == null ? <h1>No Data</h1> : quote.slip.advice}</h1>
+                        <h1 className="flex-grow sm:pr-16 text-2xl font-medium title-font text-gray-900">{id}</h1>
+                        <h1 className="flex-grow sm:pr-16 text-2xl font-medium title-font text-gray-900">{advice}</h1>
                         <button onClick={() => { fetchUserData() }} className="flex-shrink-0 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg mt-10 sm:mt-0">New Advice</button>
                     </div>
                 </div>
